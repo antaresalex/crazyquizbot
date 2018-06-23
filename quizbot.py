82303d4e2826887b609e0be811a3a02fea1d81f4
Aleksandra Kulikova <antares.alexx@gmail.com>
2018-06-23 17:16:50 +0300

    Версия с выбором параметров вопросов позьзователем и выводом вопросов последовательно в консоль

--- quizbot.py
+++ quizbot.py
@@ -17,13 +17,31 @@ def quiz (url, proxies):
 		get_response = response.json()
 		return get_response
 	else:
-		return 'На сервере что-то пошло не так.'
-
+		return 'Server is not responding now and something has broken'
 
 if __name__ == '__main__':
-	user_category = input('Выбирай скорее тему вопросов и погнали.')
-	user_difficulty = input('Кстати, хочешь посложнее или полегче вопросики?')
+	user_category = input('Choose a category of questions.')
+	user_difficulty = input('Choose a dificulty level.')
 	url = 'https://opentdb.com/api.php?amount=5&category=%s&difficulty=%s' % (user_category, user_difficulty)
-	questions = quiz(url, proxies)
+	data = quiz(url, proxies)
+
+	results = data.get('results')
+
+	for question_answer_info in results:
+		print(question_answer_info['question'])
+		answers = question_answer_info['incorrect_answers']
+		correct_answer = question_answer_info['correct_answer']
+		answers.append(correct_answer)
+		#a = answers.copy()
+		print(answers)
+		user_answer = input('Write a right answer.')
+		if user_answer == correct_answer:
+			print('Yo, bro! It is correct answer.')
+		else:
+			print("""Of course, no.
+Correct answer is """ + correct_answer +'.')
+
+
+
 	
-	print(questions)
+	
