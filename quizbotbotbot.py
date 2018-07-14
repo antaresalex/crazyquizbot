c585866b1d3d93476893c6ec17394971481a2fbe
Irina Dubrovina <dubrovina.iri@gmail.com>
2018-07-14 16:05:45 +0300

    adding  changes for bot

--- quizbotbotbot.py
+++ quizbotbotbot.py
@@ -8,27 +8,34 @@ logging.basicConfig(format='%(name)s - %(levelname)s - %(message)s',
                     )
 
 def greet_user(bot, update):
-	hello = "Hello, dear user. I am Crazy Quiz Bot. \nI know many fun questions of different topics. Let is start to play."
-	print(hello)
-	update.message.reply_text(hello)
+    hello = "Hello, dear user. I am Crazy Quiz Bot. \nI know many fun questions from all over the world. \nLet`s do it!"
+    print(hello)
+    update.message.reply_text(hello)
+
 
 def talk_to_me(bot, update):
     user_text = update.message.text 
     print(user_text)
     update.message.reply_text(user_text)
 
+
 def get_token():
-	with open('bot_token.json', 'r') as f_bot_token:
-		bot_token = f_bot_token.read()
-		return bot_token
+    with open('bot_token.json', 'r') as f_bot_token:
+        bot_token = f_bot_token.read()
+        return bot_token
+
 
 def start_bot():
-	mybot = Updater(get_token(), request_kwargs=proxy_login_data() )
-	mybot.dispatcher.add_handler(CommandHandler('start', greet_user))
-	mybot.dispatcher.add_handler(MessageHandler(Filters.text, talk_to_me))
+    PROXY = proxy_login_data()
+    mybot = Updater(get_token(), request_kwargs=PROXY)
+    dp = mybot.dispatcher
+    dp.add_handler(CommandHandler('start', greet_user))
+    dp.add_handler(CommandHandler('questions', quiz_questions_answers))
 
-	mybot.start_polling()
-	mybot.idle()
+    dp.add_handler(MessageHandler(Filters.text, talk_to_me))
+
+    mybot.start_polling()
+    mybot.idle()
 
 if __name__ == '__main__':
-	start_bot()
+    start_bot()
