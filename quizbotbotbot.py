239e26c325524c2aa032456b61a4426cf347605b
Aleksandra Kulikova <antares.alexx@gmail.com>
2018-10-14 13:16:03 +0300

    Добавлена ReplyKeyBoard

--- quizbotbotbot.py
+++ quizbotbotbot.py
@@ -1,5 +1,5 @@
-from telegram import InlineKeyboardButton, InlineKeyboardMarkup
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, ConversationHandler
+from telegram import ReplyKeyboardMarkup, InlineKeyboardButton, InlineKeyboardMarkup
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, RegexHandler, ConversationHandler
 import json
 import logging
 
@@ -36,23 +36,40 @@ def greet_user(bot, update, user_data):
     update.message.reply_text(hello)
     user_data['user_name'] = update.message.chat.first_name
     print(user_data['user_name'])
- 
+
 def choose_category(bot, update, user_data):
-    keyboard = [[InlineKeyboardButton("General Knowledge", callback_data='9')],
-        [InlineKeyboardButton("Entertaiment: Books", callback_data='10')],
-        [InlineKeyboardButton("Entertaiment: Films", callback_data='11')],
-        [InlineKeyboardButton("Entertaiment: Music", callback_data='12')],
-        [InlineKeyboardButton("Science: Computers", callback_data='18')],
-        [InlineKeyboardButton("Science & Nature", callback_data='17')],
-        [InlineKeyboardButton("Science: Mathematics", callback_data='19')],
-        [InlineKeyboardButton("History", callback_data='23')],             
-        [InlineKeyboardButton("Sports", callback_data='21')],                
-        [InlineKeyboardButton("Art", callback_data='25')],
-        [InlineKeyboardButton("Geography", callback_data='22')],
-        [InlineKeyboardButton("Animals", callback_data='27')]
-    ]
-    reply_markup = InlineKeyboardMarkup(keyboard)
+    keyboard = [['General Knowledge', 'Books', 'Films', 'Music'],
+    ['Computers', 'Science & Nature', 'Mathematics', 'History'],
+    ['Sports', 'Art', 'Geography', 'Animals']]
+    reply_markup = ReplyKeyboardMarkup(keyboard)
     update.message.reply_text('\nChoose a category of questions:', reply_markup=reply_markup)
+
+def create_url_category(bot, update, user_data):
+    dict_category = {'General Knowledge': '9', 'Books': '10', 'Films': '11', 'Music': '12', 'Computers': '18', 'Science & Nature': '17', 'Mathematics': '19', 'History': '23', 'Sports': '21', 'Art': '25', 'Geography': '22', 'Animals': '27'}
+    user_data['user_category'] = 
+
+def create_url(bot, update, user_data)
+    user_category = user_data['user_category']
+    user_difficulty = user_data['user_difficulty']
+    url = 'https://opentdb.com/api.php?amount=5&category=%s&difficulty=%s' % (user_category, user_difficulty)
+    return url
+
+# def choose_category(bot, update, user_data):
+#     keyboard = [[InlineKeyboardButton("General Knowledge", callback_data='9')],
+#         [InlineKeyboardButton("Entertaiment: Books", callback_data='10')],
+#         [InlineKeyboardButton("Entertaiment: Films", callback_data='11')],
+#         [InlineKeyboardButton("Entertaiment: Music", callback_data='12')],
+#         [InlineKeyboardButton("Science: Computers", callback_data='18')],
+#         [InlineKeyboardButton("Science & Nature", callback_data='17')],
+#         [InlineKeyboardButton("Science: Mathematics", callback_data='19')],
+#         [InlineKeyboardButton("History", callback_data='23')],             
+#         [InlineKeyboardButton("Sports", callback_data='21')],                
+#         [InlineKeyboardButton("Art", callback_data='25')],
+#         [InlineKeyboardButton("Geography", callback_data='22')],
+#         [InlineKeyboardButton("Animals", callback_data='27')]
+#     ]
+#     reply_markup = InlineKeyboardMarkup(keyboard)
+#     update.message.reply_text('\nChoose a category of questions:', reply_markup=reply_markup)
     # user_data['user_category'] = callback_data
     # print(user_data['user_category'])
     #https://www.programcreek.com/python/example/106608/telegram.ext.CallbackQueryHandler
@@ -84,6 +101,7 @@ def start_bot():
     dp = mybot.dispatcher
     dp.add_handler(CommandHandler('start', greet_user, pass_user_data=True))
     dp.add_handler(CommandHandler('play', choose_category, pass_user_data=True))
+    #dp.add_handler(RegexHandler('^(Computers)$', create_url, pass_user_data=True))
     dp.add_handler(MessageHandler(Filters.text, talk_to_me, pass_user_data=True))
 
     mybot.start_polling()
