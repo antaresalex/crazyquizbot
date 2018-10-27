9c95174e50b5a4536921ecf528a20dff603e83f0
Aleksandra Kulikova <antares.alexx@gmail.com>
2018-10-27 17:45:33 +0300

    Работающая версия с вопросами и проверкой правильности ответа

--- quizbotbotbot.py
+++ quizbotbotbot.py
@@ -1,5 +1,6 @@
 from telegram import ReplyKeyboardMarkup, ReplyKeyboardRemove, InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, RegexHandler, ConversationHandler
+import random
 import json
 import logging
 import requests
@@ -60,6 +61,7 @@ def quiz_choose_difficulty(bot, update, user_data):
 
 def quiz_url(bot, update, user_data):
     user_difficulty = update.message.text
+    user_difficulty = user_difficulty.lower()
     user_data['user_difficulty'] = user_difficulty
     user_category = user_data['user_category']
     url = 'https://opentdb.com/api.php?amount=5&category=%s&difficulty=%s' % (user_category, user_difficulty)
@@ -70,9 +72,11 @@ def quiz_url(bot, update, user_data):
         if data.get('response_code') == 0:
             results = data.get('results')
             user_data['user_results'] = results
-            return 'quiz_questions'
+            user_data['user_question_number'] = 0
+            return quiz_questions(bot, update, user_data)
         else:
             print('Something is wrong. Check Postman')
+            print(url)
             return 'quiz_restart'
     else:
         print('Server is not responding now. Check Postman')
@@ -80,36 +84,61 @@ def quiz_url(bot, update, user_data):
         #return ConversationHandler.END
 
 def quiz_questions(bot, update, user_data):
-    results = user_data['user_results']
-    for question_answer_info in results:
-        return(question_answer_info['question'])
-        answers = question_answer_info['incorrect_answers']
-        correct_answer = question_answer_info['correct_answer']
-        answers.append(correct_answer)
-        return(answers)
-        user_answer = input('Write a right answer.')
-        if user_answer == correct_answer:
-            return('Yo! It is correct answer.')
+    results = user_data['user_results'] 
+    user_question_number = user_data['user_question_number']
+    question_answer_info = results[user_question_number]
+    question_text = question_answer_info['question']
+    user_data['user_question'] = question_text
+    answers = question_answer_info['incorrect_answers']
+    correct_answer = question_answer_info['correct_answer']
+    user_data['user_correct_answer'] = correct_answer
+    answers.append(correct_answer)
+    random.shuffle(answers)
+    keyboard = []
+    for answer in answers:
+        keyboard.append([answer])
+    reply_markup = ReplyKeyboardMarkup(keyboard)
+    update.message.reply_text(question_text + '\nChoose a right answer:', reply_markup=reply_markup)
+    return 'quiz_answers'
+
+def quiz_answers(bot, update, user_data):
+    user_answer = update.message.text
+    correct_answer = user_data['user_correct_answer']
+    if user_answer == correct_answer:
+        update.message.reply_text('Yo! It is correct answer.')
+        user_question_number = user_data['user_question_number']
+        if user_question_number != 4:
+            user_data['user_question_number'] = user_question_number + 1 
+            return quiz_questions(bot, update, user_data)
         else:
-            return("""Nope. Try again.
-""" + str(answers))
-            user_answer = input('Write a right answer.')
-            if user_answer == correct_answer:
-                return('Great! This is correct.')
-            else:                       
-                return("""No, no, no.
-Correct answer is """ + correct_answer +'.')
-    user_choice_continue = input('Well done! Bro, would you want to continue?')
-    return user_choice_continue
-    
-if __name__ == '__main__':
-    answers_questions = 'yes'
-    while answers_questions == 'yes':
-        user_questions_api = user_questions_url()
-        result_quiz = quiz(user_questions_api)
-        answers_questions = quiz_questions_answers(result_quiz)
+            update.message.reply_text('Well done! Quiz is over. Bro, would you want to continue?')
+            #клавиатура с да или нет и отлавливаем ответ в рестарте
+            return quiz_restart(bot, update, user_data)
     else:
-        return 'Okey and Buy. I will be miss you.'
+        question_text = user_data['user_question']
+        update.message.reply_text('No! It is incorrect answer. Try again.\n' + question_text)
+        return 'quiz_answers'
+
+#     else:
+#         return("""Nope. Try again.
+# """ + str(answers))
+#         user_answer = input('Write a right answer.')
+#         if user_answer == correct_answer:
+#             return('Great! This is correct.')
+#         else:                       
+#             return("""No, no, no.
+# Correct answer is """ + correct_answer +'.')
+#     user_choice_continue = input('Well done! Bro, would you want to continue?')
+#     return user_choice_continue
+    
+# if __name__ == '__main__':
+#     answers_questions = 'yes'
+#     while answers_questions == 'yes':
+#         user_questions_api = user_questions_url()
+#         result_quiz = quiz(user_questions_api)
+#         answers_questions = quiz_questions_answers(result_quiz)
+#     else:
+#         return 'Okey and Buy. I will be miss you.'
 
     #https://www.programcreek.com/python/example/106608/telegram.ext.CallbackQueryHandler
 
@@ -126,26 +155,31 @@ def talk_to_me(bot, update, user_data):
 
 def dont_know(bot, update, user_data):
     user_text = update.message.text 
-    update.message.reply_text('When you write' + user_text + 'What do you mean?\nI dont understand you, sorry')
+    update.message.reply_text('When you write: ' + user_text + '. What do you mean?\nI dont understand you, sorry')
 
 def start_bot():
     mybot = Updater(get_token(), request_kwargs=proxy_login_data() )
     dp = mybot.dispatcher
-    dp.add_handler(CommandHandler('start', greet_user, pass_user_data=True))
-    dp.add_handler(CommandHandler('google', open_google, pass_user_data=True))
-    dp.add_handler(RegexHandler('^(google|info|search)$', open_google, pass_user_data=True))
     quiz = ConversationHandler(
         entry_points=[CommandHandler('play', quiz_choose_category, pass_user_data=True)],
         states={
-        'category': [CommandHandler('play', quiz_choose_category, pass_user_data=True)],
-        'difficulty': [RegexHandler('^(General Knowledge|Books|Films|Music|Computers|Science & Nature|Mathematics|History|Sports|Art|Geography|Animals)$', quiz_choose_difficulty, pass_user_data=True)],
-        'create_url': [RegexHandler('^(Easy|Medium|Hard)$', quiz_url, pass_user_data=True)],
-        'quiz_restart': [CommandHandler('restart', quiz_restart, pass_user_data=True), 
-                        RegexHandler('^(restat|again)$',quiz_restart, pass_user_data=True)]
-            },
+            'category': [CommandHandler('play', quiz_choose_category, pass_user_data=True)],
+            'difficulty': [RegexHandler('^(General Knowledge|Books|Films|Music|Computers|Science & Nature|Mathematics|History|Sports|Art|Geography|Animals)$', quiz_choose_difficulty, pass_user_data=True)],
+            'create_url': [RegexHandler('^(Easy|Medium|Hard)$', quiz_url, pass_user_data=True)],
+            'quiz_restart': [
+                CommandHandler('restart', quiz_restart, pass_user_data=True), 
+                RegexHandler('^(restat|again)$',quiz_restart, pass_user_data=True)
+            ],
+            'quiz_answers': [MessageHandler(Filters.text, quiz_answers, pass_user_data=True)]
+        },
+        #fallbacks=[]
         fallbacks=[MessageHandler(Filters.text, dont_know, pass_user_data=True)]
         )
     dp.add_handler(quiz)
+    dp.add_handler(CommandHandler('start', greet_user, pass_user_data=True))
+    dp.add_handler(CommandHandler('google', open_google, pass_user_data=True))
+    dp.add_handler(RegexHandler('^(google|info|search)$', open_google, pass_user_data=True))
+
     dp.add_handler(MessageHandler(Filters.text, talk_to_me, pass_user_data=True))
 
     mybot.start_polling()
